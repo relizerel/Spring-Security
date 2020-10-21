17df1414b81bb71d1ebe497c9fd4fd822e24e482
relizerel <relizerel@gmail.com>
2020-10-21 11:14:35 +0200

    Измнил аннотацию @EnableTransactionManagement. Все заработало

--- src/main/java/com/spring/config/DataConfig.java
+++ src/main/java/com/spring/config/DataConfig.java
@@ -20,7 +20,7 @@ import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:db.properties")
-@EnableTransactionManagement
+@EnableTransactionManagement(proxyTargetClass = true)
 @ComponentScan("com.spring")
 @EnableJpaRepositories("com.spring")
 public class DataConfig {
