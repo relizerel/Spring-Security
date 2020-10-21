96c8ecc9a74bb103c339872a86fce1a849b3d164
relizerel <relizerel@gmail.com>
2020-10-21 14:15:48 +0200

    Подготовил проект для изменения

--- src/main/java/com/spring/config/SecurityConfig.java
+++ src/main/java/com/spring/config/SecurityConfig.java
@@ -4,9 +4,7 @@ import com.spring.config.handler.LoginSuccessHandler;
 import com.spring.service.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -56,10 +54,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //страницы аутентификаци доступна всем
                 .antMatchers("/login", "/").anonymous()
                 // защищенные URL
-                /*.antMatchers("/users/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+                .antMatchers("/users/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
                 .antMatchers("/user/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
                 .antMatchers("/admin/**").access("hasAnyRole('ROLE_ADMIN')")
-                */.anyRequest().authenticated();
+                .anyRequest().authenticated();
     }
 
     @Bean
