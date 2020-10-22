d1eb0e2fbe497fb3e38a6ff74b689679185b641c
relizerel <relizerel@gmail.com>
2020-10-22 13:04:49 +0200

    Слил все в мастер

--- src/main/java/com/spring/service/UserServiceImp.java
+++ src/main/java/com/spring/service/UserServiceImp.java
@@ -22,62 +22,8 @@ public class UserServiceImp implements UserDetailsService, UserService{
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    /*private final UserDao userDao;
+    private final UserDao userDao;
 
-    public UserServiceImp(UserDao userDao) {
-        this.userDao = userDao;
-    }
-
-    @Transactional
-    @Override
-    public User getUser(Long id) {
-        return userDao.getUser(id);
-    }
-
-    @Transactional
-    @Override
-    public void saveUser(User user) {
-        userDao.saveUser(user);
-    }
-
-    @Transactional
-    @Override
-    public void updateUser(User user) {
-        userDao.updateUser(user);
-    }
-
-    @Transactional
-    @Override
-    public void deleteUser(User user) {
-        userDao.deleteUser(user);
-    }
-
-    @Override
-    public List<User> listUsers() {
-        return userDao.listUsers();
-    }
-
-    @Override
-    public User getUserByUsername(String username) {
-        return userDao.getUserByUsername(username);
-    }
-
-    @Override
-    public void createUser(User user) {
-        user.setRoles(Collections.singleton(new UserRole(1L, "ROLE_USER")));
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userDao.saveUser(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDao.getUserByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return user;
-    }*/
-    private UserDao userDao;
     @Autowired
     private UserServiceImp(UserDao userDao) {
         this.userDao = userDao;
